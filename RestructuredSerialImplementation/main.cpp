891b0bbf7dfb98c2fd302b38af65c0b19609d9c7
alonzno <alonzoa@cs.uoregon.edu>
2018-07-24 14:35:07 -0700

    Rename name to cpp, add optimization flag to make recipe

--- RestructuredSerialImplementation/main.cpp
+++ RestructuredSerialImplementation/main.cpp
@@ -1,8 +1,9 @@
 #include "Triangulation.hpp"
 
 #include <stdio.h>
+#include <sys/time.h>
 
-#define NUM_POINTS 20000
+#define NUM_POINTS 50000
 
 double *
 PointsGenerator(int numPoints, int dim = 2) {
@@ -20,12 +21,21 @@ PointsGenerator(int numPoints, int dim = 2) {
 }
 
 int main(int argc, char **argv) {
+    struct timeval start, end;
+    double runtime;
+
     Triangulation tri;
     double *pts = PointsGenerator(NUM_POINTS);
 
     tri.Initialize(NUM_POINTS, pts);
     tri.AddBoundingTri();
+
+    gettimeofday(&start, NULL);
     tri.AddPoints();
+    gettimeofday(&end, NULL);
+    runtime = end.tv_sec + (end.tv_usec / 1000000.0) - start.tv_sec - (start.tv_usec / 1000000.0);
+    cerr << runtime << endl;
+
     tri.DelBoundingTri();
     tri.WriteOutTriangleVTK((char *) "lonzo.vtk");
 
